94a25a951de2f3f89d088658ee314c060f1d1df3
Peter <peterjakemiranda@gmail.com>
2022-12-01 22:57:54 +0800

    Fix sms sending

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -27,23 +27,24 @@ class SmsController extends Controller
             $user->parent_mobile = $request->get('mobile');
             $user->save();
             $client = new \GuzzleHttp\Client();
-            $response = $client->request('GET', 'https://www.cloud.smschef.com/api/send/sms', [
+            $response = $client->request('GET', 'https://sms.pagenet.info/admin/index.php', [
                 'headers' => [
                     'Accept' => 'application/json',
                 ],
                 'query' => [
-                    'mode' => 'devices',
-                    'secret' => '3a5ac86583f6c4b34770c9f22a52992b0716b366',
-                    'device' => '00000000-0000-0000-a1b7-053ff308d107',
-                    'sim' => 1,
-                    'phone' => $request->get('mobile'),
+                    'route' => 'api/sms/send',
+                    'auth_key' => 'p6rV1tCjldQ05HCiuO8Zh5ZXtMSv44tIOG7bvHgC',
+                    'device_id' => 21,
+                    'sim_id' => 4,
+                    'mobile_no' => $request->get('mobile'),
+                    'data_type' => 'Plain',
                     'message' => $request->get('message'),
                 ]
             ]);
             info('sms response', ['data' => $response]);
+            https://sms.pagenet.info/admin/index.php?route=api/sms/send&auth_key=p6rV1tCjldQ05HCiuO8Zh5ZXtMSv44tIOG7bvHgC&device_id=21&sim_id=99&mobile_no=09488005645&data_type=Plain&message=Hello+World
             // https://sms.pagenet.info/admin/index.php?route=api/sms/send&auth_key=p6rV1tCjldQ05HCiuO8Zh5ZXtMSv44tIOG7bvHgC&device_id=18&sim_id=3&mobile_no=09665761200&data_type=Plain&message=Hello+World+test
         } catch (\Exception $e) {
-            info('exception', compact('e'));
             return response()->json(['message' => 'Failed Sending SMS'], 400);
         }
         return response()->json(['message' =>  'Successfully sent sms'], 200);
